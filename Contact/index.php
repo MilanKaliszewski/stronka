c7d6f99e91829cc32b73b6cd45df6b677be66a17
MiLaNPL <63824351+MiLaNPL@users.noreply.github.com>
2020-09-29 20:19:42 +0200

    Master

--- Contact/index.php
+++ Contact/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="icon" href="../photos/icon.png" type="image/icon type">
-		<title>Walker's P&I Design - O Nas</title>
+		<title>Walker's P&I Design - Kontakt</title>
 		<link href="../css/style.css" rel="stylesheet" type="text/css">
 		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 	</head>
@@ -12,9 +12,9 @@
 			<a href="#" class="logo"><img src="../photos/logo.png" alt="logo serwisu" class="logo"/></a>
 			<ul class="menu-items">
 				<li class="item"><a href="../index.html" class="item">Home</a></li>
-				<li class="item-active"><a href="#" class="item-active">About</a></li>
+				<li class="item"><a href="../About/index.html" class="item">About</a></li>
 				<li class="item"><a href="../Houses/index.html" class="item">Houses</a></li>
-				<li class="item"><a href="../Contact/index.php" class="item">Contact</a></li>
+				<li class="item-active"><a href="#" class="item-active">Contact</a></li>
 			</ul>
 		</nav>
 	</body>
