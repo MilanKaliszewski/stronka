7221f9d5811c39db584a26a21c91d6647b040d80
SzelsoNNNN <72217754+SzelsoNNNN@users.noreply.github.com>
2020-10-02 16:08:24 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting(0);
+session_start();
+//https://discordapp.com/api/webhooks/761281303197909033/z0-K_7i1hqhRUzmjKb0vfEvB0SC1EYkT7pWD3UQZlxQMuRYBzdK1AcU5OeTohqbiHJs8
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="icon" href="../photos/icon.png" type="image/icon type">
+		<title>Walker's P&I Design - Kontakt</title>
+		<link rel="stylesheet" href="../css/style.css">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<nav class="menu">
+			<a href="#" class="logo"><img src="../photos/logo.png" alt="logo serwisu" class="logo"/></a>
+			<ul class="menu-items">
+				<li class="item"><a href="../index.html" class="item">Home</a></li>
+				<li class="item"><a href="../About/index.html" class="item">ABOUT</a></li>
+				<li class="item"><a href="../Houses/index.html" class="item">Houses</a></li>
+				<li class="item-active"><a href="index.php" class="item-active">Contact</a></li>
+			</ul>
+		</nav>
+		<div class="fastwrapper">
+		    <img src="../photos/Cjteczh.jpg" alt="Los Santos Tower" class="banner">
+            <h1 class="hej">Dziękujemy za kontakt! <br>
+                            Wkrótce dostaniesz wiadomość zwrotną od jednego z naszych konsultantów</h1>
+		</div>
+	</body>
+</html>
