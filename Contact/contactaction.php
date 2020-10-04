3393a06417194055930d8c725a7856b2a8fe62b2
MiLaNPL <milanxpompa@gmail.com>
2020-10-04 19:12:37 +0200

    master123

---
+++ Contact/contactaction.php
@@ -0,0 +1,41 @@
+<?php
+//https://discordapp.com/api/webhooks/761281303197909033/z0-K_7i1hqhRUzmjKb0vfEvB0SC1EYkT7pWD3UQZlxQMuRYBzdK1AcU5OeTohqbiHJs8
+if(!$_POST['customer_email'] || !$_POST['customer_name'] || !$_POST['customer_surname'] || !$_POST['contact_topic'] || !$_POST['contact_content']) { 
+    $_SESSION['somethingwrong'] = "<span style='color: red; font-size: 25px;'>WPISZ WSZYSTKIE DANE!</span>";
+    header('location:index.php');
+}
+else{
+$webhookurl = "https://discordapp.com/api/webhooks/761281303197909033/z0-K_7i1hqhRUzmjKb0vfEvB0SC1EYkT7pWD3UQZlxQMuRYBzdK1AcU5OeTohqbiHJs8";
+$timestamp = date("c", strtotime("now"));
+$email = $_POST['customer_email'];
+$name = $_POST['customer_name'];
+$sname = $_POST['customer_surname'];
+$topic = $_POST['contact_topic'];
+$content = $_POST['contact_content'];
+$json_data = json_encode([
+    "content" => "@everyone",
+    "username" => "Sekretarka",
+    "tts" => false,
+    "embeds" => [
+        [
+            "title" => "IMIĘ I NAZWISKO: $name $sname",
+            "type" => "rich",
+            "description" => "**TYTUŁ:** $topic
+            **TREŚĆ:**
+            $content",
+            "url" => "$email",
+            "timestamp" => $timestamp,
+            "color" => hexdec( "3366ff" ),
+        ],
+                ]
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+$ch = curl_init( $webhookurl );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $json_data);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $ch );
+curl_close( $ch );
+header('location: contact.php');}
--- Contact/index.php
+++ Contact/index.php
@@ -1,21 +1,43 @@
+<?php
+error_reporting(0);
+session_start();
+?>
 <!doctype html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<link rel="icon" href="../photos/icon.png" type="image/icon type">
 		<title>Walker's P&I Design - Kontakt</title>
-		<link href="../css/style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="../css/style.css">
 		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 	</head>
-	<body>
+	<body style="background-image: url(../photos/tlo.png)">
 		<nav class="menu">
 			<a href="#" class="logo"><img src="../photos/logo.png" alt="logo serwisu" class="logo"/></a>
 			<ul class="menu-items">
 				<li class="item"><a href="../index.html" class="item">Home</a></li>
-				<li class="item"><a href="../About/index.html" class="item">About</a></li>
+				<li class="item"><a href="../About/index.html" class="item">ABOUT</a></li>
 				<li class="item"><a href="../Houses/index.html" class="item">Houses</a></li>
 				<li class="item-active"><a href="#" class="item-active">Contact</a></li>
 			</ul>
 		</nav>
+		<section class="formwrapper">
+		    <form action="contactaction.php" method="post" class="formularz">
+		        E-MAIL<br>
+		        <input type="text" placeholder="Link do profilu na forum" name="customer_email">
+		          <br><br>IMIĘ<br>
+		        <input type="text" placeholder="Imię IC" name="customer_name">
+		        <br><br>NAZWISKO<br>
+		        <input type="text" placeholder="Nazwisko IC" name="customer_surname">
+		        <br><br>TEMAT<br>
+		        <input type="text" placeholder="Temat" name="contact_topic">
+		        <br><br>TREŚĆ<br>
+		        <textarea name="contact_content" id="contact_content" cols="30" rows="10" placeholder="Wpisz tutaj treść swojej wiadomości" maxlength="490"></textarea>
+		        <?php 
+                echo $_SESSION['somethingwrong'];
+                ?>
+		        <br><input type="submit" value="Wyślij formularz">
+		    </form>
+		</section>
 	</body>
 </html>
